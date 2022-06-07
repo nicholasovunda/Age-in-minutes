537e3c4429aede08d1531138d1f62f2560c1bdf3
nicholas <nicholasovunda@gmail.com>
2022-06-07 11:42:46 +0100

    null safety checks using let

--- app/src/main/java/com/example/ageinminute/MainActivity.kt
+++ app/src/main/java/com/example/ageinminute/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.ageinminute
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button =  findViewById<Button>(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val year =  myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+//        implemented a date picker dialog
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+            Toast.makeText(this, "DatePicker works", Toast.LENGTH_LONG).show()
+            },
+            year,
+            month,
+            day
+            ).show()
+
+
     }
 }
